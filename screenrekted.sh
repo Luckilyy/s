51ae462c64a75228bb2d8c673113ad9d470775ac
Boulacheb Lucas <lucas.boulacheb@epitech.eu>
2016-10-14 13:50:02 +0200

    UP

--- screenrekted.sh
+++ screenrekted.sh
@@ -2,11 +2,15 @@
 while [ 1 > 0 ]
 do
 xrandr -o left
+setxkbmap us
 sleep 1
 xrandr -o inverted
+setxkbmap ru
 sleep 1
 xrandr -o right
+setxkbmap bo
 sleep 1
 xrandr -o normal
+setxkbmap fr
 sleep 1
 done
